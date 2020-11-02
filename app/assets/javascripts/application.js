40a4100da16d3f845a2ba0742fbf7a36ca5e330d
Haris Tanveer <haristanveer.ht@gmail.com>
2020-11-03 00:37:22 +0500

    make it better

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require rails_ujs
+//= require jquery
+//= require jquery_ujs
 //= require vendor.min
 //= require plugins.min
 //= require bs-stepper.min
