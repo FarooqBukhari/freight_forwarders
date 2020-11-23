b33e2e47c79400d4b8630b1fa772a26a834b2130
Haris Tanveer <haristanveer.ht@gmail.com>
2020-11-24 00:07:54 +0500

    production email changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'apikey',
+    password: "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",
+    :domain => 'https://send2world.herokuapp.com/',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
+
